8dcc181c6dc015385ebe259d4f2c6f5f1dc942d8
Dan <danielfr500@gmail.com>
2022-03-02 19:04:22 -0500

    password now prints to screen

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Simple-Javascript-Password-Generator
 Simple Javascript Password Generator
+
+
+Access the generator at: https://dfreeman500.github.io/Simple-Javascript-Password-Generator/
--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function generatePossibleCharacters() {
     let possibleCharactersArray = possibleCharacters.split('')
     charactersToAvoid = document.getElementById("charactersToAvoid").value
 
-    console.log("this is possible characters: " + possibleCharacters)
+    // console.log("this is possible characters: " + possibleCharacters)
     for (i = 0; i < charactersToAvoid.length; i++) {
         var index = possibleCharactersArray.indexOf(charactersToAvoid[i]);
         if (index !== -1) {
@@ -34,19 +34,48 @@ function generatePossibleCharacters() {
         }
 
     }
-    console.log("this is characters to avoid " + charactersToAvoid)
-    console.log("this is characters after avoiding removed " + possibleCharactersArray)
-
-    let lengthOfPassword = document.getElementById("lengthOfPassword").value
+    // console.log("this is characters to avoid " + charactersToAvoid)
+    // console.log("this is characters after avoiding removed " + possibleCharactersArray)
 
 
+    // if (possibleCharactersArray.length<1){
+    //     alert("You have to provide some characters in order for a password to be created!")
+    //     return "You have to provide some characters in order for a password to be created!"
+    // }
 
 
-
-
-    return possibleCharacters
+    return possibleCharactersArray
 }
 
 
 
 
+// Copy Button
+
+function randomGenerator(possibleCharactersArray,lengthOfPassword){
+    
+    console.log(possibleCharactersArray)
+    let randomPassword=""
+    for (i=0; i<lengthOfPassword; i++){
+        let index = Math.floor(Math.random()*possibleCharactersArray.length)
+        randomPassword += possibleCharactersArray[index]
+        console.log("i value: " + i+ ".index: "+ index+ ". item in array "+ possibleCharactersArray[index])
+
+    }
+    // if (randomPassword.length<1){
+    //     alert("Your current settings do not allow for generation of a password with any length!")
+    // }
+    return randomPassword
+}
+// randomGenerator()
+
+function createPassword(){
+    randomPassword = randomGenerator(generatePossibleCharacters(), lengthOfPassword = document.getElementById("lengthOfPassword").value)
+    if (randomPassword.length<1 || randomPassword.slice(0,9)==="undefined"){
+        randomPassword= "You have to provide enough characters or length to generate a password!"
+    }
+    console.log(randomPassword)
+    console.log("udnefined: " + randomPassword.slice(0,9))
+    let messageOutput = document.getElementById("messageOutput")
+    messageOutput.innerText = randomPassword
+}
